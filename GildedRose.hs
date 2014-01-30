39159c5c121dcd7ec9d23db47bbb102951f9a3e6
Johannes Schirrmeister <johannes.schirrmeister@prezi.com>
2014-01-30 16:02:10 +0100

    Step 3

--- GildedRose.hs
+++ GildedRose.hs
@@ -6,13 +6,19 @@ data Item = Item { name :: String, sellIn :: Int, quality :: Int }
 type Inventory = [Item]
 
 processOneDay :: Inventory -> Inventory
-processOneDay inventory = map updateQuality inventory
+processOneDay inventory = map updateItem inventory
 
-updateQuality :: Item -> Item
+updateItem :: Item -> Item
+updateItem i@Item{name, sellIn, quality} 
+    | stillValid && notDegraded = Item name (sellIn - 1) (updateQuality i)
+    | stillValid = Item name (sellIn - 1) 0
+    | notDegraded = Item name 0 (updateQuality i)
+    | otherwise = Item name 0 0
+    where stillValid = sellIn > 0
+          notDegraded = quality > 1
+
+updateQuality :: Item -> Int
 updateQuality i@Item{name, sellIn, quality} 
-	| stillValid && notDegraded = Item name (sellIn - 1) (quality - 1)
-	| stillValid = Item name (sellIn - 1) 0
-	| notDegraded = Item name 0 (quality - 2)
-	| otherwise = Item name 0 0
-	where stillValid = sellIn > 0
-	      notDegraded = quality > 1
+    | name == "AgedBrie" = quality + 1
+    | sellIn == 0 = quality - 2
+    | otherwise = quality - 1
--- GildedRoseTest.hs
+++ GildedRoseTest.hs
@@ -8,21 +8,26 @@ standardItem = Item "testItem" 5 5
 sellInReachedItem = Item "testItem" 0 5
 fullyDegradedsellInReachedItem = Item "testItem" 0 0
 
-qualityDecreasesByOne = Test (quality (updateQuality standardItem) == 4) "An item that needs to be sold in 2 days, needs to be sold in 1 day tomorrow"
-sellInDecreasesByOne = Test (sellIn (updateQuality standardItem) == 4) "An item with quality 2 will have quality 1 tomorrow"
-sellInReachedSellInRemainsZero = Test (sellIn (updateQuality sellInReachedItem) == 0) "The sellIn value cannot go below 0"
-sellInReachedAndFullyDegraded = Test (sellIn (updateQuality sellInReachedItem) == 0) "The sellIn value cannot go below 0 even if quality is 0"
+qualityDecreasesByOne = Test (quality (updateItem standardItem) == 4) "An item that needs to be sold in 2 days, needs to be sold in 1 day tomorrow"
+sellInDecreasesByOne = Test (sellIn (updateItem standardItem) == 4) "An item with quality 2 will have quality 1 tomorrow"
+sellInReachedSellInRemainsZero = Test (sellIn (updateItem sellInReachedItem) == 0) "The sellIn value cannot go below 0"
+sellInReachedAndFullyDegraded = Test (sellIn (updateItem sellInReachedItem) == 0) "The sellIn value cannot go below 0 even if quality is 0"
 
 -- Step 1
-sellInReachedQualityDegradesFaster = Test (quality (updateQuality sellInReachedItem) == 3) "The quality of an item that already perished degrades twice as fast"
+sellInReachedQualityDegradesFaster = Test (quality (updateItem sellInReachedItem) == 3) "The quality of an item that already perished degrades twice as fast"
 
 -- Step 2
 fullyDegradedItem = Item "testItem" 2 0
-fullyDegraded = Test (quality (updateQuality fullyDegradedItem) == 0) "The quality of an item does not go below 0"
+fullyDegraded = Test (quality (updateItem fullyDegradedItem) == 0) "The quality of an item does not go below 0"
+
+-- Step 3
+agedBrie = Item "AgedBrie" 3 3
+agedBrieQualityIncreases = Test (quality (updateItem agedBrie) == 4) "The quality of AgedBrie increases"
 
 gildedRoseTest = [qualityDecreasesByOne,
 				  sellInDecreasesByOne,
 				  sellInReachedSellInRemainsZero,
 				  sellInReachedAndFullyDegraded,
 				  sellInReachedQualityDegradesFaster,
-				  fullyDegraded]
+				  fullyDegraded,
+				  agedBrieQualityIncreases]
